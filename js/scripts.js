c404ecbb07dba37b6ce250dcccf20d6b43365c7f
AG977 <93376305+AG977@users.noreply.github.com>
2021-11-09 22:39:44 +0100

    added pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Lasagne';
-document.write(favoriteFood);
+// array of objects
+let pokemonlist = [
+  { name: 'Pikachu', heigt: 0.4, types: ['electric']},
+  { name: 'Moltres', heigt: 2, types: ['fire', 'flying']},
+  { name: 'Lugia', heigt: 5.2, types:['psychic', 'flying']},
+];
