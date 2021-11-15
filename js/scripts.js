5e7146d38cefe545529ae152f33a218887d6be82
AG977 <93376305+AG977@users.noreply.github.com>
2021-11-15 19:58:34 +0100

    Added IIFE and fixed loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 // array of objects
 
-(function () {
-let pokemonlist = [
-  { name: 'Pikachu', heigt: 0.4, types: ['electric']},
-  { name: 'Moltres', heigt: 2, types: ['fire', 'flying']},
-  { name: 'Lugia', heigt: 5.2, types:['psychic', 'flying']},
-]})();
+let pokemonRepository = (function () {
+let pokemonList = [
+  { name: 'Pikachu', height: 0.4, types: ['electric']},
+  { name: 'Moltres', height: 2, types: ['fire', 'flying']},
+  { name: 'Lugia', height: 5.2, types:['psychic', 'flying']},
+];
+
+function getAll() {
+  return pokemonList;
+}
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
 
 // foreach loop
 document.write('<ul>')
 function myLoopFunction(pokemon) {
 document.write('<li>')
-document.write(pokemon.name + pokemon.height);
+document.write(pokemon.name  +  pokemon.height);
 if (pokemon.height > 3){
 document.write(" Wow, that’s big! ")
   }
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
 document.write('</li>')
 document.write('</ul>')
