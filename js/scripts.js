46c380ec1d9e14b4ac2ab6294504ea3acb916226
AG977 <93376305+AG977@users.noreply.github.com>
2021-11-15 18:55:56 +0100

    created foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,28 @@ let pokemonlist = [
   { name: 'Moltres', heigt: 2, types: ['fire', 'flying']},
   { name: 'Lugia', heigt: 5.2, types:['psychic', 'flying']},
 ];
+
+
+//foreach loop
+document.write('<ul>')
+pokemonList.forEach(function(pokemon) {
+document.write('<li>')
+document.write(pokemon.name + pokemon.height);
+if (pokemon.height > 3){
+document.write(" Wow, that’s big! ")
+document.write('</li>')
+document.write('</ul>')
+}});
+
+// foreach loop made clearer
+document.write('<ul>')
+function myLoopFunction(pokemon) {
+document.write('<li>')
+document.write(pokemon.name + pokemon.height);
+if (pokemon.height > 3){
+document.write(" Wow, that’s big! ")
+  }
+}
+pokemonList.forEach(myLoopFunction);
+document.write('</li>')
+document.write('</ul>')
