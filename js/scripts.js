eb84d62fbc1c85c27ed9836959419dcfe525a642
AG977 <93376305+AG977@users.noreply.github.com>
2021-11-16 12:46:34 +0100

    corrected loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 // array of objects
-let pokemonlist = [
-  { name: 'Pikachu', heigt: 0.4, types: ['electric']},
-  { name: 'Moltres', heigt: 2, types: ['fire', 'flying']},
-  { name: 'Lugia', heigt: 5.2, types:['psychic', 'flying']},
+let pokemonList = [
+  { name: 'Pikachu', height: 0.4, types: ['electric']},
+  { name: 'Moltres', height: 2, types: ['fire', 'flying']},
+  { name: 'Lugia', height: 5.2, types:['psychic', 'flying']},
 ];
 
 
 // foreach loop
 document.write('<ul>')
-function myLoopFunction(pokemon) {
-document.write('<li>')
-document.write(pokemon.name + pokemon.height);
-if (pokemon.height > 3){
-document.write(" Wow, that’s big! ")
+  function myLoopFunction(pokemon) {
+        document.write('<li>')
+              document.write(pokemon.name + pokemon.height);
+              if (pokemon.height > 3){
+              document.write(" Wow, that’s big! ")
   }
 }
-pokemonList.forEach(myLoopFunction);
-document.write('</li>')
+              pokemonList.forEach(myLoopFunction)
+        document.write('</li>')
 document.write('</ul>')
